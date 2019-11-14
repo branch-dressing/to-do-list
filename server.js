22d912c42d7556988f9b26fbf58dcc65df8b73e0
joelpdurham <joeldur@gmail.com>
2019-11-14 10:13:05 -0800

    drop and add tables work. not auth to see them on the browser though.

--- server.js
+++ server.js
@@ -4,8 +4,31 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const client = require('./lib/client');
-
 client.connect();
+
+const ensureAuth = require('./lib/auth/ensure-auth');
+const createAuthRoutes = require('./lib/auth/create-auth-routes');
+const authRoutes = createAuthRoutes({
+    selectUser(email) {
+        return client.query(`
+        SELECT id, email, hash, display_name as "displayName"
+        FROM users
+        WHERE email = $1
+        `,
+        [email]
+        ).then(result => result.rows[0]);
+    },
+    insertUser(user, hash) {
+        console.log(user);
+        return client.query(`
+        INSERT into users (email, hash, display_name)
+        VALUES ($1, $2, $3)
+        RETURNING id, email, display_name as "displayName";
+        `,
+        [user.email, hash, user.displayName]
+        ).then(result => result.row[0]);
+    }
+});
 
 // Application Setup
 const app = express();
@@ -15,7 +38,16 @@ app.use(cors()); // enable CORS request
 app.use(express.static('public')); // server files from /public folder
 app.use(express.json()); // enable reading incoming json data
 
+app.use('/api/auth', authRoutes);
+app.use('/api', ensureAuth);
+
 // API Routes
+
+app.get('/api/test', (req, res) => {
+    res.json({
+        message: `the user's id is ${req.userId}`
+    });
+});
 
 // *** TODOS ***
 app.get('/api/todos', async (req, res) => {
@@ -41,8 +73,8 @@ app.post('/api/todos', async (req, res) => {
 
     try {
         const result = await client.query(`
-            INSERT INTO todos (task, complete)
-            VALUES ($1, $2)
+            INSERT INTO todos (task, complete, user_id)
+            VALUES ($1, $2, $3)
             RETURNING *;
         `,
         [todo.task, todo.complete]);
